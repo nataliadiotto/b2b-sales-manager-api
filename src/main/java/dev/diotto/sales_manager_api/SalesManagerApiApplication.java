696f4331867982fc9ddd73b5bca21d53d27bf8c6
nataliadiotto <diottonatalia@gmail.com>
2025-08-18 09:30:04 -0300

    first commit

---
+++ src/main/java/dev/diotto/sales_manager_api/SalesManagerApiApplication.java
@@ -0,0 +1,13 @@
+package dev.diotto.sales_manager_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/diotto/sales_manager_api/SalesManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.diotto.sales_manager_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
