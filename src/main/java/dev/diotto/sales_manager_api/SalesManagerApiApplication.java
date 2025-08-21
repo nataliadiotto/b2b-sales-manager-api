153b0068e05fc54bb0bbc8a6e39c2e33876c1322
nataliadiotto <diottonatalia@gmail.com>
2025-08-21 08:03:31 -0300

    feat: database config

--- src/main/java/dev/diotto/sales_manager_api/SalesManagerApiApplication.java
+++ src/main/java/dev/diotto/sales_manager_api/SalesManagerApiApplication.java
@@ -2,12 +2,14 @@ package dev.diotto.sales_manager_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class SalesManagerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SalesManagerApiApplication.class, args);
+		System.out.println("Hello World");
 	}
 
 }
