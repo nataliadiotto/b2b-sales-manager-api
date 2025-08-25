e06f3d1ebba494aa9babd9ff5fab8be6cb365aa1
nataliadiotto <diottonatalia@gmail.com>
2025-08-25 09:36:35 -0300

    feat: add createdAt and updatedAt timestamps for Product

--- src/main/java/dev/diotto/sales_manager_api/domain/entity/Product.java
+++ src/main/java/dev/diotto/sales_manager_api/domain/entity/Product.java
@@ -2,20 +2,33 @@ package dev.diotto.sales_manager_api.domain.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "products")
-@Getter @Setter
-@Data
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
-    private String name;
-    private String description;
-    private BigDecimal unitPrice;
+    private Long id;
+
+    @Setter private String name;
+    @Setter private String description;
+    @Setter private BigDecimal unitPrice;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
 }
