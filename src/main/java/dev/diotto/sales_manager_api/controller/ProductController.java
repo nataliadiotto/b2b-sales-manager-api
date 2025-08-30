5212b44f25fcdf937ccc84572dbac5175b2d2df7
nataliadiotto <diottonatalia@gmail.com>
2025-08-30 17:16:37 -0300

    feat: implement deletedeleteProductById in ProductController

--- src/main/java/dev/diotto/sales_manager_api/controller/ProductController.java
+++ src/main/java/dev/diotto/sales_manager_api/controller/ProductController.java
@@ -3,6 +3,7 @@ package dev.diotto.sales_manager_api.controller;
 import dev.diotto.sales_manager_api.domain.dto.ProductDTO;
 import dev.diotto.sales_manager_api.domain.dto.ProductResponseDTO;
 import dev.diotto.sales_manager_api.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,7 +23,7 @@ public class ProductController {
     }
 
     @PostMapping
-    public ResponseEntity<ProductResponseDTO> createProduct(@RequestBody ProductDTO productDTO) {
+    public ResponseEntity<ProductResponseDTO> createProduct(@RequestBody @Valid ProductDTO productDTO) {
         ProductResponseDTO productResponseDTO = productService.createProduct(productDTO);
 
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -42,6 +43,13 @@ public class ProductController {
         ProductResponseDTO productResponseDTO = productService.findProductById(id);
 
         return ResponseEntity.ok(productResponseDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProductById(@PathVariable Long id) {
+        productService.deleteProduct(id);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
