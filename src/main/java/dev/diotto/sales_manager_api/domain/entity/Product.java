b2ec77a6ea485bbeca1332de37634ed7dd0494f4
nataliadiotto <diottonatalia@gmail.com>
2025-08-31 15:18:28 -0300

    feat: add validation messages

--- src/main/java/dev/diotto/sales_manager_api/domain/entity/Product.java
+++ src/main/java/dev/diotto/sales_manager_api/domain/entity/Product.java
@@ -1,6 +1,10 @@
 package dev.diotto.sales_manager_api.domain.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -19,9 +23,9 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Setter private String name;
-    @Setter private String description;
-    @Setter private BigDecimal unitPrice;
+    @Setter @NotBlank @Size(min = 2, max = 255) private String name;
+    @Setter @NotBlank @Size(min = 2, max = 255) private String description;
+    @Setter @NotNull @DecimalMin(value = "0.01") private BigDecimal unitPrice;
 
     @Column(name = "created_at", nullable = false, updatable = false)
     @CreationTimestamp
