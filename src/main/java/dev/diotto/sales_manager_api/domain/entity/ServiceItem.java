fb783ffe2730596e16c7ffecf67ba6af5e3fb8fd
nataliadiotto <diottonatalia@gmail.com>
2025-08-31 18:55:46 -0300

    feat: create ServiceItem

---
+++ src/main/java/dev/diotto/sales_manager_api/domain/entity/ServiceItem.java
@@ -0,0 +1,44 @@
+package dev.diotto.sales_manager_api.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "service_items")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter @NotBlank @Size(min = 2, max = 255) private String name;
+    @Setter @NotBlank @Size(min = 2, max = 255) private String description;
+    @Setter @NotNull @DecimalMin(value = "0.01") private BigDecimal unitPrice;
+    @Setter @NotBlank @Size(min = 2, max = 255) private String duration; // e.g., "Per Hour", "Per Month"
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    private boolean active = true;
+
+}
