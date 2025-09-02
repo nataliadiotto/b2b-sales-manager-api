dd2531f2a06a564c66cb3d705edbbb737805f354
nataliadiotto <diottonatalia@gmail.com>
2025-09-02 05:18:49 -0300

    feat: add validation messages basename to application properties

--- src/main/java/dev/diotto/sales_manager_api/domain/entity/Company.java
+++ src/main/java/dev/diotto/sales_manager_api/domain/entity/Company.java
@@ -8,6 +8,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "companies")
@@ -23,8 +27,16 @@ public class Company {
     @Setter @NotBlank @Size(min = 2, max = 255) private String legalName;
     @Setter @NotBlank @Size(min = 2, max = 255) private String cnpj;
     @Setter @NotBlank @Size(min = 2, max = 255) @Email private String email;
-    @Setter @NotBlank @Size(min = 2, max = 255) private String address;
+    @Setter @Embedded private Address address;
     @Setter @NotBlank @Size(min = 2, max = 255) private String phone;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
 
 }
